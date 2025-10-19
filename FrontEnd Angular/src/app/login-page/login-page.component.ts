be69fc624b365d92aa1de552f8df19bcf71a477d
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2025-10-19 16:52:49 -0500

    Se agregan los import

--- FrontEnd Angular/src/app/login-page/login-page.component.ts
+++ FrontEnd Angular/src/app/login-page/login-page.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { EmpresaService } from '../services/empresa.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css'] 
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  email: string = '';
+  passwordIngreso: string = '';
+  erroMsg: string = '';
 
-  // 
-  constructor (private router: Router) {}
+  //
+  constructor(private router: Router, private empresaService: EmpresaService) {}
 
   //Método para ridirigir a Register
   goRegister(event: Event, tipo: string) {
-    event.preventDefault()
-    this.router.navigate(['/register'])
+    event.preventDefault();
+    this.router.navigate(['/register']);
     console.log('Redirigiendo a:', tipo);
+  }
+
+  onLogin() {
+    this.empresaService.login(this.email, this.passwordIngreso).subscribe({
+      next: (response) => {
+        console.log('Login exitoso ✅', response);
+        //Guardar info en localStorage es alternativo
+        // localStorage.setItem('empresa', JSON.stringify(response.empresa));
+        //Redirigir a panel de gestion
+        this.router.navigate(['/panel-de-gestion', response.empresa._id]);
+      },
+      error: (err) => {
+        console.error('Error al iniciar sesión ❌', err);
+        this.erroMsg = err.error?.message || 'Usuario no registrado';
+      },
+    });
   }
 }
