728c93dc1cdb05c266555f4fca0367ea77bf4d26
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2025-10-19 16:41:37 -0500

    se creo un nuevo endpoint para poder hacer login he ingresar a la aplicación

--- BackEnd/src/routers/empresa.route.ts
+++ BackEnd/src/routers/empresa.route.ts
@@ -2,13 +2,17 @@ import express from 'express';
 
 import {
     crearEmpresa,
-    consultaNombreEmpresa
+    consultaNombreEmpresa,
 } from '../controllers/empresa.controllers';
+
+import {loginEmpresa } from '../controllers/loginEmpresa.controllers';
 
 const router = express.Router();
 
 router.post('/', crearEmpresa);
 
 router.get('/:id', consultaNombreEmpresa);
+
+router.post('/login',loginEmpresa )
 
 export default router;
