63db87a489edddcb52f12741eb2c9064986b467a
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2025-10-19 16:45:54 -0500

    Modificamos el nombre de la ruta panel de gestion y creamos la de crear facturas

--- FrontEnd Angular/src/app/app.routes.ts
+++ FrontEnd Angular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { PanelDeGestionComponent } from './panel-de-gestion/panel-de-gestion.component';
+import { CrearFacturasComponent } from './crear-facturas/crear-facturas.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
     {path: 'login', component: LoginPageComponent},
     {path: 'register', component: RegisterPageComponent},
-    {path: 'panel-gestion/:id', component: PanelDeGestionComponent}
+    {path: 'panel-de-gestion/:id', component: PanelDeGestionComponent},
+    {path: 'crear-facturas', component: CrearFacturasComponent }
 ];
