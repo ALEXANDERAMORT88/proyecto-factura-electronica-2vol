833a662c3449128ef445272498701c085f29f2fe
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2025-10-19 16:56:52 -0500

    Se diseña la lógica para la función goFactura

--- FrontEnd Angular/src/app/panel-de-gestion/panel-de-gestion.component.ts
+++ FrontEnd Angular/src/app/panel-de-gestion/panel-de-gestion.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EmpresaService, IEmpresa} from '../services/empresa.service'
-import { from } from 'rxjs';
-import { NgIf } from "@angular/common";
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmpresaService, IEmpresa } from '../services/empresa.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-panel-de-gestion',
@@ -12,22 +11,28 @@ import { NgIf } from "@angular/common";
   styleUrls: ['./panel-de-gestion.component.css'],
 })
 export class PanelDeGestionComponent implements OnInit {
-  
   empresa?: IEmpresa;
 
-  constructor (
-     private route: ActivatedRoute,
-    private empresaService: EmpresaService
+  constructor(
+    private route: ActivatedRoute,
+    private empresaService: EmpresaService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-      const id = this.route.snapshot.paramMap.get('id');
-      console.log('ID de la empresa:', id);
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('ID de la empresa:', id);
 
-      if (id) {
-        this.empresaService.obtenerEmpresa(id).subscribe(data => {
-          this.empresa = data;
-        })
-      }
+    if (id) {
+      this.empresaService.obtenerEmpresa(id).subscribe((data) => {
+        this.empresa = data;
+      });
+    }
+  }
+
+  goFactura(event: Event, tipo: string) {
+    event?.preventDefault();
+    this.router.navigate(['/crear-facturas']);
+    console.log('Redirigiendo a: ', tipo);
   }
 }
